dbcf087e5693640163692134ca63d719edf21708
datadecayvps <samsteashop@outlook.com>
2025-05-09 14:21:55 +0000

    yes

--- Ultraviolet-Static/public/data/splash.js
+++ Ultraviolet-Static/public/data/splash.js
@@ -2,5 +2,13 @@ var splashtexts = [
     "These splashtexts have been commandeered by DataDecay",
     "NOBODY CARES ABOUT COLORMAGIC",
     ":3",
-    "<span style='color:red'>WHO WANTS AI COLORS??? NOBODY</span>"
+    "<span style='color:red'>WHO WANTS AI COLORS??? NOBODY</span>",
+    "All ads removed by me, DataDecay",
+    "If someone gave you this link other than request.datadecay.dev; go there now and request a link. (password: secrettoken)",
+    "very yes",
+    "I love it not being ad-ridden",
+    "Tinf0il: March Edition",
+    "you found the secret",
+    "you should visit <a href='https://multiproxy.datadecay.dev'>multiproxy</a> to get more proxies",
+    "OwO"
 ]
