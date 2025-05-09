ed08093012b4b6f38a45c77f3e3de8e489ac289a
datadecayvps <samsteashop@outlook.com>
2025-05-09 17:50:16 +0000

    yes

--- Ultraviolet-Static/public/data/splashtexts.js
+++
@@ -1,6 +0,0 @@
-var splashtexts = [
-    "These splashtexts have been commandeered by DataDecay",
-    "NOBODY CARES ABOUT COLORMAGIC",
-    ":3",
-    "<span style='color:red'>WHO WANTS AI COLORS??? NOBODY</span>"
-]
--- Ultraviolet-Static/public/js/script.js
+++ Ultraviolet-Static/public/js/script.js
@@ -121,5 +121,5 @@ function search(input, template) {
   
     // Attempts to convert the input to a fully qualified URL have failed
     // Treat the input as a search query
-    return `https://www.google.com/search?q=${encodeURIComponent(input)}`
+    return `https://www.bing.com/search?q=${encodeURIComponent(input)}`
   }
